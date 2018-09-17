02c334b418f6f8af605c7938d568b5f00d063dac
Forketlolpro <ilyacheburov@yandex.ru>
2018-09-18 00:30:21 +0300

    heroku test

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(bodyparser.json());
+app.use('/dist',express.static('dist'));
 
-app.use(express.static(__dirname+'/dist'));
-
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
+console.log("Server start on port "+port+"   Dirname= "+__dirname);
